064a836e0a2874e50b5231a68f52d2c44fcc61ee
egee-irl <brian@egee.io>
2019-02-13 23:38:00 -0800

    added support for tags & password

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 resource "linode_instance" "instance" {
+  tags             = "${var.tags}"
   label            = "${var.label}"
   image            = "${var.image}"
   region           = "${var.region}"
   type             = "${var.type}"
+  root_pass        = "${var.root_pass}"
   authorized_keys  = "${var.authorized_keys}"
   authorized_users = "${var.authorized_users}"
   private_ip       = "${var.private_ip}"
